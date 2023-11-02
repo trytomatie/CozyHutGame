e06fd2e1e7d4b7500a436a04692b2df95a15b02c
trytomatie <trytomatie@gmail.com>
2023-11-02 16:30:57 +0100

    Refactor 4

--- Assets/Malbers Animations/Common/Scripts/Core/Interfaces/IDeltaRootMotion.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MalbersAnimations
-{
-    /// <summary> Interface to reset all rootMotion delta values from the Animal Controller  </summary>
-    public interface IDeltaRootMotion 
-    {
-        void ResetDeltaRootMotion();
-    } 
-}
