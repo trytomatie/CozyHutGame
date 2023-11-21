5f0370c0a1755e102cc96d8cb31cefc6c58f3fa7
trytomatie <trytomatie@gmail.com>
2023-11-21 01:37:25 +0100

    Adjusted Grass settings LOD,

--- Project/Assets/Scripts/FixedSkinnedMeshRendererBones.cs
+++ Project/Assets/Scripts/FixedSkinnedMeshRendererBones.cs
@@ -7,13 +7,18 @@ public class FixedSkinnedMeshRendererBones : MonoBehaviour
     public SkinnedMeshRenderer targetMeshRenderer;
     public SkinnedMeshRenderer originMeshRenderer;
     public Transform rootBone;
+    public Transform[] targetBones;
+    public Transform[] originBones;
 
     // Start is called before the first frame update
     void Start()
     {
-        SkinnedMeshRenderer spawnedPrefab = Instantiate(targetMeshRenderer, transform);
-        spawnedPrefab.bones = originMeshRenderer.bones;
-        spawnedPrefab.rootBone = rootBone;
+
+        targetBones = targetMeshRenderer.bones;
+        originBones = originMeshRenderer.bones;
+        targetMeshRenderer.bones = originMeshRenderer.bones;
+        targetMeshRenderer.rootBone = rootBone;
+
     }
 
     // Update is called once per frame
