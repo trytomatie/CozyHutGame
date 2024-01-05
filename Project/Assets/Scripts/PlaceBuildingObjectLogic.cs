bb64ff1ce2de6a6b72d57ec0003e1e0ace2e0119
trytomatie <trytomatie@gmail.com>
2024-01-05 11:32:14 +0100

    Main Island Moiuntain start, Sapplings now grow into randomized Trees

--- Project/Assets/Scripts/PlaceBuildingObjectLogic.cs
+++ Project/Assets/Scripts/PlaceBuildingObjectLogic.cs
@@ -6,13 +6,14 @@ public class PlaceBuildingObjectLogic : MonoBehaviour
 {
     public void PlaceObject(int resourceId)
     {
+        float rndScale = Random.Range(1, 1.5f);
         transform.Rotate(Vector3.right, 90f, Space.Self);
         ResourceObjectData data = new ResourceObjectData()
         {
             resource_id = resourceId,
             positon = transform.position + new Vector3(0, 0.25f, 0),
-            roation = transform.eulerAngles,
-            scale = Vector3.one,
+            roation = transform.eulerAngles + new Vector3(0,Random.Range(0,360),0),
+            scale = new Vector3(rndScale, rndScale, rndScale),
             hp = ResourceManager.instance.resources[resourceId].GetComponentInChildren<ResourceController>().maxHp,
             maxhp = ResourceManager.instance.resources[resourceId].GetComponentInChildren<ResourceController>().maxHp
         };
