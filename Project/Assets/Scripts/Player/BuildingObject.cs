6bf87f972e826891ce9a76bc1fe125be93c7b2e6
trytomatie <trytomatie@gmail.com>
2024-02-01 12:59:43 +0100

    Added Thumbnail auto Select Button for Building assets, and set Thumnails

--- Project/Assets/Scripts/Player/BuildingObject.cs
+++ Project/Assets/Scripts/Player/BuildingObject.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Netcode;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "BuildingObject/Building Object")]
@@ -16,3 +17,44 @@ public class BuildingObject : ScriptableObject
     public Item[] buildingMaterials;
     public int[] buildingMaterialAmounts;
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(BuildingObject))]
+[CanEditMultipleObjects]
+public class BuildingObjectEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        BuildingObject[] assets = Selection.GetFiltered<BuildingObject>(SelectionMode.Assets);
+        //asset.meshReference = (Mesh)EditorGUILayout.ObjectField("Mesh Reference", asset.meshReference, typeof(Mesh), false);
+        //asset.thumbnail = (Sprite)EditorGUILayout.ObjectField("Thumbnail", asset.thumbnail, typeof(Sprite), false);
+        //asset.material = (Material)EditorGUILayout.ObjectField("Material", asset.material, typeof(Material), false);
+        //asset.hasSkin = EditorGUILayout.Toggle("Has Skin", asset.hasSkin);
+        if (GUILayout.Button("Set Thumbnail", GUILayout.Height(40)))
+        {
+            SetThumbnail(assets);
+        }
+    }
+
+    private void SetThumbnail(BuildingObject[] assets)
+    {
+        foreach (BuildingObject asset in assets)
+        {
+            // Look for the Sprite in the Assets folder
+            Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>($"Assets/Screenshots/{asset.buildingPrefab.name}.png");
+            asset.sprite = sprite;
+            if (sprite != null)
+            {
+                Debug.Log($"Loaded Sprite for{asset.name}: {sprite} as thumbnail");
+            }
+            else
+            {
+                Debug.LogError($"Could not find Sprite for {asset.name}");
+            }
+            // Set dirty to save changes
+            EditorUtility.SetDirty(asset);
+        }
+    }
+}
+#endif
