3129e378419bc660baa16fe62e4fa2471520eb71
Trytomatie <trytomatie@gmail.com>
2024-04-01 04:48:20 +0200

    NPC Interaction

--- Project/Assets/Scripts/Interactable_StarNPC.cs
+++ Project/Assets/Scripts/Interactable_StarNPC.cs
@@ -3,24 +3,52 @@ using MoreMountains.Feedbacks;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Interactable_StarNPC : Interactable
 {
-    public Item[] requiredItems;
-    public int[] amounts;
-    public NetworkObject networkObject;
-    public override void ServerInteraction(ulong id)
+    public Item requiredItem;
+    public int amount;
+
+    public int recievedAmount = 0;
+
+    public Image itemImage;
+    public TextMeshProUGUI itemAmount;
+
+    private void Start()
     {
-        // Open UI to Trade Items
+        if (IsServer) UpdateUIClientRpc();
     }
 
-    public void Despawn()
+    public override void ServerInteraction(ulong id)
     {
-        networkObject.Despawn(true);
+        // Check if the player has the required items
+        if (GameManager.GetLocalPlayer().GetComponent<NetworkPlayerInit>().inventory.GetAmmountOfItem(requiredItem.itemId) > 0)
+        {
+            int playerAmount = GameManager.GetLocalPlayer().GetComponent<NetworkPlayerInit>().inventory.GetAmmountOfItem(requiredItem.itemId);
+            int amountToGive = Mathf.Clamp(playerAmount,0,amount - recievedAmount);
+            recievedAmount += amountToGive;
+            GameManager.GetLocalPlayer().GetComponent<NetworkPlayerInit>().inventory.RequestRemoveItemServerRpc(new Item.ItemData(requiredItem.itemId, amountToGive));
+            print("That MF had the Items!");
+        }
+        else
+        {
+            print(source.name + " didn't have the items");
+        }
+        UpdateUIClientRpc();
     }
+
+    [ClientRpc]
+    private void UpdateUIClientRpc()
+    {
+        itemImage.sprite = requiredItem.sprite;
+        itemAmount.text = recievedAmount + "/" + amount;
+    }
+
 
 
 }
